b9117801f7fcb01304b019d04322d0694dfc501b
shenbagalakshmi <shenbasl@gmail.com>
2018-02-23 23:11:19 +0530

    Add files via upload

--- src/app/track-home/track-home.component.ts
+++ src/app/track-home/track-home.component.ts
@@ -1,25 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import{Router} from '@angular/router';
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
 
+import { ScriptLoaderService } from '../_services/script-loader.service';
+import {Helpers} from '../helpers'
 @Component({
   selector: 'app-track-home',
   templateUrl: './track-home.component.html',
-  styleUrls: ['./track-home.component.css']
+  styleUrls: ['./track-home.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TrackHomeComponent implements OnInit {
 
 
    perentPrgs:number= 34;
+   globalBodyClass = 'm-page--loading-non-block m-page--fluid m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default';
+
   //  hypertrackString:string="https://dashboard.hypertrack.com/widget/map/users?key=pk_test_a54fc32e7d3325e61bdde2be60f627c4ac7a977f";
-  constructor(private router:Router) { }
+  constructor(private router:Router,private _script:ScriptLoaderService) { }
 
   ngOnInit() {
-  }
 
+    this._script.loadScripts('body', ['assets/vendors/base/vendors.bundle.js', 'assets/demo/demo10/base/scripts.bundle.js'], true)
+    .then(result => {
+        Helpers.setLoading(false);
+        // optional js to be loaded once
+        // this._script.loadScripts('head', ['assets/vendors/custom/fullcalendar/fullcalendar.bundle.js'], true);
+    });
+
+    this.router.events.subscribe((route) => {
+      if (route instanceof NavigationStart) {
+          Helpers.setLoading(true);
+          Helpers.bodyClass(this.globalBodyClass);
+      }
+      if (route instanceof NavigationEnd) {
+          Helpers.setLoading(false);
+      }
+  });
+
+   
+  }
 
   showLocation(){
     // window.location.href="https://dashboard.hypertrack.com/widget/map/users?key=pk_test_a54fc32e7d3325e61bdde2be60f627c4ac7a977f";
 
     this.router.navigate(['/userMgmt']);
+  }
+
+
+  addClassNvTrigger(){
+    document.getElementById("aNav").classList.add("nav-is-visible");
+  }
+
+
+  removeNav(){
+    if(document.getElementById("aNav").classList.contains("nav-is-visible")){
+      document.getElementById("aNav").classList.remove("nav-is-visible")
+    }
+
   }
 }
