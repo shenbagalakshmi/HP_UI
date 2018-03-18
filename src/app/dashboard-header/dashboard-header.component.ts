41004b8bdc48fa03db2030758e8ac6ed5b5206d2
shenbagalakshmi <shenbasl@gmail.com>
2018-03-18 22:40:05 +0530

    Add files via upload

--- src/app/dashboard-header/dashboard-header.component.ts
+++ src/app/dashboard-header/dashboard-header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 import { AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Router, NavigationStart, NavigationEnd,NavigationError } from '@angular/router';
+
 
 
 declare let mLayout: any;
@@ -10,13 +12,32 @@ declare let mLayout: any;
 })
 export class DashboardHeaderComponent implements OnInit,AfterViewInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
     // 
   }
   ngAfterViewInit(){
     mLayout.initHeader();
+  }
+
+
+
+  goToLive(){
+    this.router.navigate(['/mapmodule']);
+  }
+
+
+  showLive(){
+    document.getElementById("dashboardDiv").style.display="none"
+    document.getElementById("liveLocationDiv").style.display="block";
+    document.getElementById("teamLocationDiv").style.display="none";
+  }
+
+  showDashboard(){
+    document.getElementById("dashboardDiv").style.display="block"
+    document.getElementById("liveLocationDiv").style.display="none";
+    document.getElementById("teamLocationDiv").style.display="none";
   }
  
 }
